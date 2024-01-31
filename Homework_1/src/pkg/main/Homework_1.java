7434b3380df7eb5bb9fa8227b8011e253839e09f
Justice Mitchell <justice.s.mitchell@gmail.com>
2024-01-30 23:55:41 -0600

    Adds Justice's name and github ID

--- Homework_1/src/pkg/main/Homework_1.java
+++ Homework_1/src/pkg/main/Homework_1.java
@@ -5,6 +5,8 @@ public class Homework_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Name: Kaitlyn\nID: 117764408\n");
+		
+		System.out.println("Name: Justice\nID: jus_mitchell);
 	}
 
 }
